ce1f12f430f7df5280d9dad0794cc3c20c2a90a3
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-11-09 18:13:47 +0800

    Progress: Add Condition When After Login and & Logout

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -8,6 +8,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link href="<?= BASEURL; ?>assets/css/style.css" rel="stylesheet">
 </head>
 
 <style>
@@ -26,18 +27,26 @@
     }
 </style>
 
-<body class="bg-dark">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark py-3" data-bs-theme="dark">
+<body>
+    <nav class="navbar navbar-expand-lg py-3" style="background-color: rgba(24, 27, 25, 1); border-bottom: 1px solid rgba(255, 255, 255, 0.12);">
         <div class="container">
-            <a class="navbar-brand fw-bold" href="<?= BASEURL; ?>">Recting</a>
+            <a class="navbar-brand fw-bold" href="<?= BASEURL; ?>">
+                <img src="<?= BASEURL; ?>assets/img/brand-logo.svg" alt="Brand Logo" class="img-fluid brand-logo" width="62">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav d-flex justify-content-between align-items-center w-100">
                     <a class="nav-link" href="<?= BASEURL; ?>">Home</a>
-                    <input type="text" class="form-control" placeholder="Searching movie" style="width: 300px !important;">
-                    <a class="nav-link" href="<?= BASEURL; ?>login">Login</a>
+                    <form action="" method="post">
+                        <input type="text" class="input" placeholder="Searching movie.." style="width: 300px !important;">
+                    </form>
+                    <?php if (empty($_SESSION['user'])) { ?>
+                        <a class="nav-link" href="<?= BASEURL; ?>login">Login</a>
+                    <?php } else { ?>
+                        <a class="nav-link" href="<?= BASEURL; ?>login">Logout</a>
+                    <?php } ?>
                 </div>
             </div>
         </div>
